0afeec6da9f7913efb8a5b702b3fd47a63238aff
yangce <yangce@hzjt108.com>
2020-03-26 16:31:46 +0800

    项目初始化

---
+++ pages/test/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="page">
+    测试页面
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" type="text/less">
+</style>
